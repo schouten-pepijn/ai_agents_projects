254f135959a77688dab7eca53e4c17256f79c8d5
Pepijn Schouten <pepijn.schouten@flynth.nl>
2025-09-13 23:48:56 +0200

    start llm_stock_forecaster

---
+++ ai_stock_forecaster/src/tools/finance_yf.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import yfinance as yf
+
+
+def get_daily(symbol: str, period="max", interval="1d") -> pd.DataFrame:
+    ticker = yf.Ticker(symbol)
+    df = ticker.history(period=period, interval=interval, auto_adjust=True)
+
+    if df.empty:
+        raise RuntimeError(
+            f"No data found for symbol: {symbol} with period: {period} and interval: {interval} and yFinance"
+        )
+
+    df = df.rename(
+        columns={
+            "Open": "open",
+            "High": "high",
+            "Low": "low",
+            "Close": "close",
+            "Volume": "volume",
+        }
+    )
+
+    return df[
+        [
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume",
+        ]
+    ].sort_index()
